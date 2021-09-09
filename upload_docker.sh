761b2e40aa79997b56feccf994645491e22a2eda
abobakr9 <abobakrcr7@gmail.com>
2021-09-09 23:09:18 +0200

    starter code that similar to project 4

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=abobakr99/capstone
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login --username=abobakr99 &&\
+    docker image tag api $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
